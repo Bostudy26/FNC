64394465fbe91a324e01e1e4e68c90b877ac74ce
Bo <boprojectbo@gmail.com>
2024-09-07 19:50:30 +0200

    Feat: convex

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -6,18 +6,30 @@ const Heroes = () => {
             <div className="flex items-center">
                 <div className="relative h-[300px] w-[300px] sm:h-[350px] sm:w-[350px] md:h-[400px] md:w-[400px]">
                     <Image 
+                        src="/documents.png"
+                        fill
+                        className="object-contain dark:hidden"
+                        alt="Document"
+                    />
+                    <Image 
                         src="/documents-dark.png"
                         fill
-                        className="object-contain"
+                        className="object-contain hidden dark:block"
                         alt="Document"
                     />
                 </div>
                 <div className="relative h-[400px] w-[400px] hidden md:block">
                     <Image 
-                            src="/reading-dark.png"
-                            fill
-                            className="object-contain"
-                            alt="Document"
+                        src="/reading.png"
+                        fill
+                        className="object-contain dark:hidden"
+                        alt="Reading"
+                    />
+                    <Image 
+                        src="/reading-dark.png"
+                        fill
+                        className="object-contain hidden dark:block"
+                        alt="Reading"
                     />
                 </div>
 
--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -12,10 +12,18 @@ const Logo = () => {
     return ( 
             <div className="hidden md:flex items-center gap-x-2">
                 <Image 
+                    src="/logo.svg"
+                    height="40"
+                    width="40"
+                    alt="Logo"
+                    className="dark:hidden"
+                />
+                <Image 
                     src="/logo-dark.svg"
                     height="40"
                     width="40"
                     alt="Logo"
+                    className="dark:block"
                 />
                 <p className={cn("font-semibold", font.className)}>
                     Botion
