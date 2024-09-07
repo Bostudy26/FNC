a2cecb0dd689dc879e33956851bb55124bfb37c1
Bo <boprojectbo@gmail.com>
2024-09-07 17:24:57 +0200

    feat: heading/heroes

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,15 @@
+import Heading from "./_components/heading";
 
 
-export default function Home() {
-  return (
-      <p>
-        Hello Notion
-      </p>
-  );
+const MarketingPage = () => {
+  return ( 
+        <div className="min-h-full flex flex-col">
+          <div className="flex flecx-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+            <Heading />
+            <Heroes />
+          </div>
+        </div> 
+        );
 }
+ 
+export default MarketingPage;
