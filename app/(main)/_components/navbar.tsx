dddddaddca77bcae22998d5e4a221ebf6a9f4c9c
Bo <boprojectbo@gmail.com>
2024-09-09 10:35:21 +0200

    Feat: menu + skeletion

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import { useQuery } from "convex/react";
+import { useParams } from "next/navigation";
+import { MenuIcon } from "lucide-react";
+
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 
-import { useQuery } from "convex/react";
-import { MenuIcon } from "lucide-react";
-import { useParams } from "next/navigation";
 import Title from "./title";
 import Banner from "./banner";
+import Menu from "./menu";
 
 interface NavbarProps {
     isCollapsed: boolean;
@@ -26,8 +28,11 @@ const Navbar = ({
 
     if(document === undefined){
         return (
-            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-3 w-full flex items-center">
+            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-3 w-full flex items-center justify-between">
                 <Title.Skeletion/>
+                <div className="flex items-center gap-x-2">
+                    <Menu.Skeletion/>
+                </div>
             </nav>
         )
     }
@@ -49,6 +54,11 @@ const Navbar = ({
                     <Title
                         initialData={document}
                     />
+                    <div className="flex items-center gap-x-2">
+                        <Menu
+                            documentId={document._id}
+                        />
+                    </div>
                 </div>
             </nav>
             {document.isArchived && (
